f6d80f4e5efdf14b635157d8e03728aab35d7fb7
kyle2501@gmail.com <kyle2501@gmail.com>
2024-07-10 16:45:55 -0700

    update serverFile_Science-Travel-xyz.go

--- Go Software /html-web-app /go-code /serverFile_Science-Travel-xyz.go
+++ Go Software /html-web-app /go-code /serverFile_Science-Travel-xyz.go
@@ -18,6 +18,174 @@ func app_welcome_center_page() {
 
 
 
+// . appHandler, ~ for Data Pages °
+func appHandler(w http.ResponseWriter, r *http.Request) {
+// ,  ° . +
+    if r.URL.Path != "/app" {
+    	http.NotFound(w, r)
+    	return
+    }
+    
+// ,
+  pageTitle := "~ www.Science-Travel.xyz - // - Website App"
+  pagePath := r.URL.Path
+  
+  pageType := ".."
+  
+// ,  ° . +
+  pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  if pagePath == "/settings" {
+      pageTitle = "Settings Page"
+      pageList = pageList
+  }
+  
+  
+// ,  ° . + 
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  appHandler
+
+
+
+
+// . pageHandler, ~ for User Pages °
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+// ,  ° . +
+
+  pageTitle := "~ www.Science-Travel.xyz - // - Website App"
+  pagePath := r.URL.Path
+  pageType := ".."
+
+
+// ,  ° . +
+pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  
+  // ,  ° . +
+  if pagePath == "/user" {
+      pageTitle = "User Page"
+      pageList = pageList
+  }
+  
+  // ,  ° . +
+  if pagePath == "/user" {
+      pageTitle = "User Page"
+      pageList = pageList
+  }
+  
+  if pagePath == "/account" {
+      pageTitle = "Account Page"
+      pageList = pageList
+  }
+  
+  if pagePath == "/profile" {
+      pageTitle = "Profile Page"
+      pageList = pageList
+  }
+  
+  
+// ,  ° . +
+  if pagePath == "/portfolio" {
+      pageTitle = "Portfolio Page"
+      pageList = pageList
+  }
+  
+  if pagePath == "/resume" {
+      pageTitle = "Resume Page"
+      pageList = pageList
+  }
+
+
+// ,  ° . +
+pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  pageHandler
+
+
+
+// . indexHandler,  ~ for Public Pages °
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+// ,  ° . +
+    if r.URL.Path != "/" {
+    	http.NotFound(w, r)
+    	return
+    }
+
+// , ° . +
+  pageTitle := "~ www.Science-Travel.xyz - // - Website App"
+  pagePath := r.URL.Path
+  pageType := ".."
+
+
+// ,  ° . +
+pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  
+  if pagePath == "/" {
+      pageTitle = "Index Page"
+      pageList = pageList
+  }
+  
+    if pagePath == "/front" {
+      pageTitle = "Front Page"
+      pageList = pageList
+  }
+  
+    if pagePath == "/main" {
+      pageTitle = "Main Page"
+      pageList = pageList
+  }
+  
+    if pagePath == "/home" {
+      pageTitle = "Home Page"
+      pageList = pageList
+  }
+  
+    if pagePath == "/start" {
+      pageTitle = "Start Page"
+      pageList = pageList
+  }
+
+// ,  ° . +
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  indexHandler
+
+
 
 //  .  html url routes 
 //  .  as well as terminal cli logs
